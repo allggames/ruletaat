434ef67d3f4aa692d768def32bbd6f1fe22b7e56
TINI <agostinaanahi2001@gmail.com>
2026-04-22 12:45:45 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+const SITE_CONFIG = {
+    source: 'atenea',
+    chatUrl: 'https://www.casinoatenea.com/',
+    prizes: {
+        'Un BONO del 100%👀': '¡Hola! Gané un BONO del 100% en el Raspa y Gana 🎫, quiero reclamarlo!',
+        'Un BONO del 150%🤯': '¡Hola! Gané un BONO del 150% en el Raspa y Gana 🎫, quiero reclamarlo!',
+        'Un BONO del 200%🍀': '¡Hola! Gané un BONO del 200% en el Raspa y Gana 🎫, quiero reclamarlo!',
+        'Un BONO A ELECCIÓN💰': '¡Hola! Gané un BONO A ELECCIÓN en el Raspa y Gana 🎫, quiero reclamarlo!',
+        '3000 FICHAS GRATIS💸': '¡Hola! Gané 3000 FICHAS GRATIS en el Raspa y Gana 🎫, quiero reclamarlo!'
+    }
+};
